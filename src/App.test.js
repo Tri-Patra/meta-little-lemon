59404039a0f250d30ce0fa5d19840ddb429d9f36
Triparna Patra <87798869+Tri-Patra@users.noreply.github.com>
2025-07-25 13:02:19 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Test to check if the Navbar component renders correctly
+test("renders Navbar component", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const navbarElement = screen.getAllByText('Home'); // Assuming Navbar has a role of "navigation"
+  expect(navbarElement[0]).toBeInTheDocument();
+});
+
+
+// Test to check if the Footer component renders correctly
+test("renders Footer component", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const footerElement = screen.getByRole("contentinfo"); // Assuming Footer has a role of "contentinfo"
+  expect(footerElement).toBeInTheDocument();
 });
